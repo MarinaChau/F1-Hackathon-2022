43079fc5ba7d247485a79863d2804330d16d9384
noual <61806677+noual@users.noreply.github.com>
2022-02-23 02:25:18 +0100

    Modif weather dataset

--- src/WeatherDataset.py
+++ src/WeatherDataset.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 
-
 class WeatherDataset:
 
     def __init__(self, sequence_length, df, batch_size, instanciate_data=True):
@@ -16,16 +15,10 @@ class WeatherDataset:
         self.batch_size = batch_size
         self.offsets = np.array([5, 10, 15, 30, 60])*60
         if instanciate_data:
-          self.x_array, self.y_categories_array, self.y_probabilities_array = self.process_inputs(
+          self.x_array, self.y_categories_array, self.y_probabilities_array = self.__process_inputs(
               df)
-        
-    def import_data(self, x, y1, y2):
-      self.x_array = x
-      self.n_features = x[0].shape[0]
-      self.y_categories_array = y1
-      self.y_probabilities_array = y2
 
-    def process_inputs(self, df):
+    def __process_inputs(self, df):
         """Generates input array as well as two label arrays from an input DataFrame.
 
         Args:
@@ -51,32 +44,12 @@ class WeatherDataset:
             # Array of features, labels are created later.
             offset_label1, offset_label2 = [], []
             for offset in self.offsets:
-                # print(f"Processing for offset {offset}...")
-                # print(f"Current time : {current_time[i]}")
-                # print(df["CURRENT_TIME"].iloc[i])
-                sess_id = df["M_SESSION_LINK_IDENTIFIER"].iloc[i]
-                # print(sess_id)
-                candidates = df[df["M_SESSION_LINK_IDENTIFIER"] == sess_id]["CURRENT_TIME"] 
-                # print(candidates)
-                # print(candidates - (df["CURRENT_TIME"].iloc[i] + offset))
-                indx = np.argmin(np.abs(candidates - (df["CURRENT_TIME"].iloc[i] + offset)))
-                # session["M_SESSION_DURATION"] - session["M_SESSION_TIME_LEFT"]
-                # indx = np.argmin(df[df["M_SESSION_LINK_IDENTIFIER"] == sess_id]["CURRENT_TIME"] - (df["CURRENT_TIME"].iloc[i] + offset))
-                # print(indx)
-                # indx = np.abs(
-                #     current_time - (current_time[i] + offset)).argmin()
-                # print(f"Index of GT: {indx}")
-                # print(df.iloc[indx])
-                m_weather = df["M_WEATHER"].iloc[indx]
+                sess_id = df["M_SESSION_LINK_IDENTIFIER"].iloc[i]  # Finding current session id
+                candidates = df[df["M_SESSION_LINK_IDENTIFIER"] == sess_id]["CURRENT_TIME"]  # Finding rows from the same session in the dataset
+                indx = np.argmin(np.abs(candidates - (df["CURRENT_TIME"].iloc[i] + offset)))  # Fetching the row correspoding with time offset i
+                m_weather = df["M_WEATHER"].iloc[indx]  # Finding target label value
                 offset_onehot = np.zeros(6)
                 offset_onehot[m_weather] = 1
-                # print(f"GT m_weather : {np.argmax(offset_onehot)}")
-
-                # if m_weather != 5:  
-                #     offset_onehot[m_weather] = 1
-                # else:  # There are only 0, 1, 2, 5
-                #     offset_onehot[3] = 1
-                # offset_rain_percentage = np.zeros(1)
                 offset_rain_percentage = df['M_RAIN_PERCENTAGE'].iloc[indx]/100
                 offset_label1.append(offset_onehot)
                 offset_label2.append(offset_rain_percentage)
@@ -85,29 +58,48 @@ class WeatherDataset:
         
         return np.array(X), np.array(label1), np.array(label2)[:, :, np.newaxis]
 
+    def __build_data(self, array, sequence_length, stride=1, shuffle=False):
+        return tf.keras.utils.timeseries_dataset_from_array(data=array,
+                                                            targets=None,
+                                                            sequence_length=sequence_length,
+                                                            sequence_stride=1,
+                                                            shuffle=shuffle,
+                                                            batch_size=self.batch_size,
+                                                            end_index=array.shape[0]-8*self.sequence_length-1)
+
+    def pad_sequence(self, array):
+      """
+      Pads a sequence to the correct number of time steps using 0s
+      """
+      shape = array.shape[1:]
+      print(shape)
+      out = np.zeros((self.sequence_length, *shape))
+      out[self.sequence_length-array.shape[0]:] = array
+      return out
+
+    def import_data(self, x, y1, y2):
+        """
+        Imports external data. Useful for loading pickled datasets.
+        """
+        self.x_array = x
+        self.n_features = x[0].shape[0]
+        self.y_categories_array = y1
+        self.y_probabilities_array = y2
+
     def create_dataset(self):
         """
         Generates dataset.
         Uses class attributes to create self.data which is a tf.data.Dataset.
         """
-        self.x = self.build_data(
+        self.x = self.__build_data(
             self.x_array, sequence_length=self.sequence_length)
-        self.y_categories = [self.build_data(
+        self.y_categories = [self.__build_data(
             np.roll(self.y_categories_array[:,i], self.sequence_length, axis=0), sequence_length=1) for i in range(5)]
         self.y_categories = [e.map(
             lambda x: tf.reshape(tf.squeeze(x), [self.batch_size, 6])) for e in self.y_categories]
-        self.y_probabilities = self.build_data(
+        self.y_probabilities = self.__build_data(
             np.roll(self.y_probabilities_array, self.sequence_length, axis=0), sequence_length=1)
         self.y_probabilities = self.y_probabilities.map(
             lambda x: tf.squeeze(x))
         self.data = tf.data.Dataset.zip(
             (self.x, (*self.y_categories, self.y_probabilities)))
-
-    def build_data(self, array, sequence_length, stride=1, shuffle=False):
-        return tf.keras.utils.timeseries_dataset_from_array(data=array,
-                                                            targets=None,
-                                                            sequence_length=sequence_length,
-                                                            sequence_stride=int(self.sequence_length/2),
-                                                            shuffle=shuffle,
-                                                            batch_size=self.batch_size,
-                                                            end_index=array.shape[0]-8*self.sequence_length-1)
